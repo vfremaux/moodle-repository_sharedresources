068d8144254efd83fc87716ebc7308b3ab972698
valery <valery.fremaux@gmail.com>
2018-03-05 11:15:39 +0100

    publish to editors

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018012500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018030400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'repository_sharedresources'; // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_RC; 
-$plugin->release = "3.3.0 (Build 2018012500)";
+$plugin->maturity = MATURITY_RC;
+$plugin->release = "3.3.0 (Build 2018030400)";
 $plugin->dependencies = array('mod_sharedresource' => 2018011801);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.3.0002';
+$plugin->codeincrement = '3.3.0004';
