189f2dbe13ffe0956f4c91e144be9847233193b1
valery <valery.fremaux@gmail.com>
2018-03-27 12:56:17 +0200

    fix lib call

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018030400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018030500;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900;        // Requires this Moodle version.
 $plugin->component = 'repository_sharedresources'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = "3.2.0 (Build 2018030400)";
+$plugin->release = "3.2.0 (Build 2018030500)";
 $plugin->dependencies = array('mod_sharedresource' => 2018011801);
 
 // Non moodle attributes.
